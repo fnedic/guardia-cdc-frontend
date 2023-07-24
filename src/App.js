8762d719c6b57f6a124c916f57950148a3aea9b7
Facu Nedic <facunedic@gmail.com>
2023-07-24 16:21:56 -0300

    protocolForm update

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import { SignBar } from "./components/public/SignBar";
 import Footer from "./components/public/Footer";
 import Dashboard from "./components/secured/Dashboard";
 import LoggedAppBar from "./components/secured/LoggedAppBar";
+import ProtocolForm from "./components/secured/ProtocolForm";
 import { CssBaseline } from "@mui/material";
 import DashAppBar from "./components/secured/DashAppBar";
 
 function App() {
   return (
     <BrowserRouter>
-    <CssBaseline/>
+      <CssBaseline />
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<LoggedAppBar />} />
         <Route path="/dashboard" element={<DashAppBar />} />
+        <Route path="/protocolUpload" element={<DashAppBar />} />
         <Route path="/register" element={<SignBar />} />
         <Route path="/login" element={<SignBar />} />
       </Routes>
       <Routes>
+        <Route path="/protocolUpload" element={<ProtocolForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LogIn />} />
